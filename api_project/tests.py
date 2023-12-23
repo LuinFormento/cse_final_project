962935dc9eaac9b6b6a7f34e4eb01064f127b090
LuinFormento <formentoluin08@gmail.com>
2023-12-23 22:20:59 +0800

    add test.py and modify .gitignore

---
+++ api_project/tests.py
@@ -0,0 +1,24 @@
+import unittest
+import warnings
+from api import app
+
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 10)
+        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+
+    def test_getactors(self):
+        response = self.app.get("/clients")
+        self.assertEqual(response.status_code, 10)
+        self.assertTrue("Reynard Surmon" in response.data.decode())
+
+
+if __name__ == "__main__":
+    unittest.main()
